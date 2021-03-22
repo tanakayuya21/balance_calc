5ca67eca38289df65bf4f9860b9a83228ee1066c
田中勇也 <tanakayuya19880503@gmail.com>
2021-03-23 01:56:10 +0900

    ファイル分け、一括加算処理

--- main.go
+++ main.go
@@ -1,61 +1,97 @@
 package main
 
 import (
-	"strconv"
+	// "fmt"
+	 "strconv"
+	// "strings"
+    // "golang.org/x/text/language"
+    // "golang.org/x/text/message"
+	// "humanize"
     "github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
+	// "github.com/jinzhu/gorm"
     _ "github.com/mattn/go-sqlite3"
+	"github.com/Users/tanakayuya/workspace/balance_calc/Model"
+
+
 )
 
 func main() {
     router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
 
-	dbInit()
-
-	// data := "Hello Go/Gin!!"
-
-    // router.GET("/", func(ctx *gin.Context){
-    //     ctx.HTML(200, "index.html", gin.H{"data": data})
-    // })
+    router.LoadHTMLGlob("View/*.html")
+    Model.DbInit()
     router.GET("/", func(ctx *gin.Context) {
-        todos := dbGetAll()
+        user_balances := Model.DbGetAll()
         ctx.HTML(200, "index.html", gin.H{
-            "todos": todos,
+            "user_balances": user_balances,
         })
     })
 
-    //Create
+    // 新規作成
     router.POST("/new", func(ctx *gin.Context) {
-        text := ctx.PostForm("text")
-        status := ctx.PostForm("status")
-        dbInsert(text, status)
+        name := ctx.PostForm("name")
+        balance := ctx.PostForm("balance")
+		balanceNumber, _ := strconv.Atoi(balance)
+        Model.DbInsert(name, balanceNumber)
         ctx.Redirect(302, "/")
     })
 
-    //Detail
+	router.GET("/addition_all", func(ctx *gin.Context) {
+		ctx.HTML(200, "addition_all.html", gin.H{})
+	})
+
+    //　編集画面
     router.GET("/detail/:id", func(ctx *gin.Context) {
         n := ctx.Param("id")
         id, err := strconv.Atoi(n)
         if err != nil {
             panic(err)
         }
-        todo := dbGetOne(id)
-        ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+        user_balance := Model.DbGetOne(id)
+        ctx.HTML(200, "detail.html", gin.H{"user_balance": user_balance})
     })
 
-    //Update
-    router.POST("/update/:id", func(ctx *gin.Context) {
-        n := ctx.Param("id")
-        id, err := strconv.Atoi(n)
-        if err != nil {
-            panic("ERROR")
-        }
-        text := ctx.PostForm("text")
-        status := ctx.PostForm("status")
-        dbUpdate(id, text, status)
-        ctx.Redirect(302, "/")
+	//Detail
+    router.GET("/edit/", func(ctx *gin.Context) {
+        ctx.HTML(200, "edit.html", gin.H{})
     })
+
+	//Update
+	router.POST("/update/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+
+		if err != nil {
+			panic("ERROR")
+		}
+		user_balance := Model.DbGetOne(id)
+
+		btn_value := ctx.PostForm("btn_value")
+
+		edit_value := ctx.PostForm("edit_value")
+		edit_value_number, _ := strconv.Atoi(edit_value)
+		
+		result_number := 0
+		if btn_value == "増やす"{
+			result_number = user_balance.Balance + edit_value_number
+		}else if btn_value == "減らす"{
+			if user_balance.Balance >= edit_value_number{
+				result_number = user_balance.Balance - edit_value_number
+			}else{
+				// panic("ERROR")
+				result_number = 0
+			}
+		}
+		Model.DbUpdate(id, result_number)
+		ctx.Redirect(302, "/")
+	})
+	//Detail
+	router.POST("/update_all", func(ctx *gin.Context) {
+		edit_value := ctx.PostForm("edit_value")
+		edit_value_number, _ := strconv.Atoi(edit_value)
+		Model.DbUpdateAll(edit_value_number)
+		ctx.Redirect(302, "/")
+	})
 
     //削除確認
     router.GET("/delete_check/:id", func(ctx *gin.Context) {
@@ -64,8 +100,8 @@ func main() {
         if err != nil {
             panic("ERROR")
         }
-        todo := dbGetOne(id)
-        ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+        user_balance := Model.DbGetOne(id)
+        ctx.HTML(200, "delete.html", gin.H{"user_balance": user_balance})
     })
 
     //Delete
@@ -75,85 +111,9 @@ func main() {
         if err != nil {
             panic("ERROR")
         }
-        dbDelete(id)
+        Model.DbDelete(id)
         ctx.Redirect(302, "/")
 
     })
     router.Run()
-}
-
-type Todo struct {
-    gorm.Model
-    Text   string
-    Status string
-}
-
-//DB初期化
-func dbInit() {
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
-    if err != nil {
-        panic("データベース開けず！（dbInit）")
-    }
-    db.AutoMigrate(&Todo{})
-    defer db.Close()
-}
-
-//DB追加
-func dbInsert(text string, status string) {
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
-    if err != nil {
-        panic("データベース開けず！（dbInsert)")
-    }
-    db.Create(&Todo{Text: text, Status: status})
-    defer db.Close()
-}
-
-//DB更新
-func dbUpdate(id int, text string, status string) {
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
-    if err != nil {
-        panic("データベース開けず！（dbUpdate)")
-    }
-    var todo Todo
-    db.First(&todo, id)
-    todo.Text = text
-    todo.Status = status
-    db.Save(&todo)
-    db.Close()
-}
-
-//DB削除
-func dbDelete(id int) {
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
-    if err != nil {
-        panic("データベース開けず！（dbDelete)")
-    }
-    var todo Todo
-    db.First(&todo, id)
-    db.Delete(&todo)
-    db.Close()
-}
-
-//DB全取得
-func dbGetAll() []Todo {
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
-    if err != nil {
-        panic("データベース開けず！(dbGetAll())")
-    }
-    var todos []Todo
-    db.Order("created_at desc").Find(&todos)
-    db.Close()
-    return todos
-}
-
-//DB一つ取得
-func dbGetOne(id int) Todo {
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
-    if err != nil {
-        panic("データベース開けず！(dbGetOne())")
-    }
-    var todo Todo
-    db.First(&todo, id)
-    db.Close()
-    return todo
 }
